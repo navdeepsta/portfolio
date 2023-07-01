fb8cbf5999b924ca43ebd5dd910f5eb1b0f587bc
navdeep <navdeepst.1987@gmail.com>
2023-07-01 18:33:03 +1000

    Revert "Implement routing and add navbar"
    This reverts commit 59fbf492360252f8430ba536adf0ed046393acd9.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import { Routes, Route } from 'react-router'
+import logo from './logo.svg';
 import './App.css';
-import About from './components/About'
-import Projects from './components/Projects'
-import Navbar from './components/Navbar';
-import Contact from './components/Contact'
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<About />}></Route>
-        <Route path='about' element={<About />}></Route>
-        <Route path='projects' element={<Projects />}></Route>
-        <Route path='contact' element={<Contact />}></Route>
-     </Routes>
-    </>   
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
