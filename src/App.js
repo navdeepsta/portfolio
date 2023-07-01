8bfe851a98a9730b376226db48d5baf1976bbc25
navdeep <navdeepst.1987@gmail.com>
2023-07-01 18:43:16 +1000

    Implement routing and add navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router'
 import './App.css';
-
+import About from './components/About'
+import Projects from './components/Projects'
+import Navbar from './components/Navbar';
+import Contact from './components/Contact'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<About />}></Route>
+        <Route path='about' element={<About />}></Route>
+        <Route path='projects' element={<Projects />}></Route>
+        <Route path='contact' element={<Contact />}></Route>
+      </Routes>
+    </>   
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-md navbar-light bg-light">
+      <div className="container-fluid">
+          <a className="navbar-brand" href="#"><span className="narbar-brand-logo">N</span></a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
+            <ul className="navbar-nav ">
+              <li className="nav-item">
+                <Link to="/about" className="nav-link active" aria-current="page" href="#">About</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/projects" className="nav-link" href="#">Projects</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact" className="nav-link" href="#">Contact</Link>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Badges</a>
+              </li>
+            </ul>
+        </div>
+      </div>
+    </nav>
+  
+  )
+}
